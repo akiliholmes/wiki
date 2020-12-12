7ca53cb848ab0d46d5fd5cc090d4b10a18b08490
akiliholmes <akili.holmes@gmail.com>
2020-12-12 10:06:41 -0800

    Updated index.html, urls.py, views.py. Saving changes.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,9 @@
+import re, markdown2
+
 from django.shortcuts import render
+from django.http import Http404, HttpResponse, HttpResponseNotFound
+from django.urls import reverse
+from django import forms
 
 from . import util
 
@@ -8,3 +13,20 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    f = util.get_entry(title)
+    if f == None:
+        raise Http404("Entry does not exist")
+    else:
+        return render(request, "encyclopedia/entry.html",{
+            "content": markdown2.markdown(f)
+        })
+
+def search(request, q):
+    e = util.list_entries()
+    c = re.compile(e, re.IGNORECASE)
+    results = re.finditer(q)
+    if results is not None:
+        return render(request, "encyclopedia/search.html", {
+            "results": results
+        })
